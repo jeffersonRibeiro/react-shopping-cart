3b8031f4f9dc5fc3c688f15a365c0313c8cd46f6
Jefferson Ribeiro <jefferson.ribeiro.contato@gmail.com>
2018-07-14 00:43:39 -0300

    v2

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 
 
 import Shelf from '../components/shelf/Shelf';
-import Banner from '../components/Banner';
+// import Banner from '../components/Banner';
 import Footer from '../components/Footer';
 import FloatCart from './../components/floatCart/FloatCart';
 
@@ -21,7 +21,7 @@ class App extends Component {
       <Provider store={store}>
         <div className="App">
           <Corner />
-          <Banner src={bannerImage} alt='Promoção Camisas do Timão' />
+          {/* <Banner src={bannerImage} alt='Promoção Camisas do Timão' /> */}
           <main>
             <Shelf />
           </main>
