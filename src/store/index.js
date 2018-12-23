f65cf5cba7a0aaeaf60a1384e47e3ade5e83a6a8
Jefferson Ribeiro <jefferson.ribeiro.contato@gmail.com>
2018-12-23 21:40:23 -0200

    disabled Redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -9,10 +9,10 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  // compose(
+  applyMiddleware(...middleware)
+  //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // )
 );
 
 export default store;
