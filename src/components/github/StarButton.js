7db93d718b6dde1cba208144ffa068a8307a74a1
Jefferson Ribeiro <jefferson.ribeiro.contato@gmail.com>
2019-02-09 19:45:38 -0200

    Update StarButton.js

--- src/components/github/StarButton.js
+++ src/components/github/StarButton.js
@@ -3,15 +3,10 @@ import React from 'react';
 const StarButton = () => (
   <div className="star-button-container">
     <small>Leave a star on Github if this repository was useful :)</small>
-    <a
-      className="github-button"
-      href="https://github.com/jeffersonRibeiro/react-shopping-cart"
-      data-size="large"
-      aria-label="Star jeffersonRibeiro/react-shopping-cart on GitHub"
-    >
-      Star
-    </a>
+    <a class="github-button" href="https://github.com/jeffersonRibeiro/react-shopping-cart" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star jeffersonRibeiro/react-shopping-cart on GitHub">Star</a>
   </div>
 );
 
 export default StarButton;
+
+
