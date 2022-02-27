f9516fdb17f553eb940dac5834ef82de1c910c28
Jefferson Ribeiro <jefferson.silva@luizalabs.com>
2022-02-27 01:32:47 -0300

    refactor: prevent cart-context to change state objects by reference when modifying product quantity

--- src/contexts/cart-context/index.tsx
+++ src/contexts/cart-context/index.tsx
@@ -27,27 +27,37 @@ const CartProvider: FC = ({ children }) => {
   const [products, setProducts] = useState<ICartProduct[]>([]);
   const [total, setTotal] = useState<ICartTotal>(totalInitialValues);
 
-  const openCart = () => {
-    setIsOpen(true);
-  };
+  const openCart = () => setIsOpen(true);
 
-  const closeCart = () => {
-    setIsOpen(false);
+  const closeCart = () => setIsOpen(false);
+
+  const updateQuantitySafely = (
+    currentProduct: ICartProduct,
+    targetProduct: ICartProduct,
+    quantity: number
+  ): ICartProduct => {
+    if (currentProduct.id === targetProduct.id) {
+      return Object.assign({
+        ...currentProduct,
+        quantity: currentProduct.quantity + quantity,
+      });
+    } else {
+      return currentProduct;
+    }
   };
 
   const addProduct = (newProduct: ICartProduct) => {
-    let productAlreadyInCart = false;
-    const updatedProducts = products.map((product: ICartProduct) => {
-      if (product.id === newProduct.id) {
-        product.quantity += 1;
-        productAlreadyInCart = true;
-      }
+    let updatedProducts;
+    const isProductAlreadyInCart = products.some(
+      (product: ICartProduct) => newProduct.id === product.id
+    );
 
-      return product;
-    });
-
-    if (!productAlreadyInCart) {
-      updatedProducts.push(newProduct);
+    if (isProductAlreadyInCart) {
+      updatedProducts = products.map((product: ICartProduct) => {
+        return updateQuantitySafely(product, newProduct, newProduct.quantity);
+      });
+    } else {
+      updatedProducts = [...products, newProduct];
     }
 
     setProducts(updatedProducts);
@@ -55,36 +65,29 @@ const CartProvider: FC = ({ children }) => {
   };
 
   const removeProduct = (productToRemove: ICartProduct) => {
-    const index = products.findIndex(
-      (product: ICartProduct) => product.id === productToRemove.id
+    const updatedProducts = products.filter(
+      (product: ICartProduct) => product.id !== productToRemove.id
     );
-    if (index >= 0) {
-      products.splice(index, 1);
-      updateCartTotal(products);
-    }
+
+    setProducts(updatedProducts);
+    updateCartTotal(updatedProducts);
   };
 
   const increaseProductQuantity = (productToIncrease: ICartProduct) => {
     const updatedProducts = products.map((product: ICartProduct) => {
-      if (product.id === productToIncrease.id) {
-        product.quantity += 1;
-      }
-
-      return product;
+      return updateQuantitySafely(product, productToIncrease, +1);
     });
 
+    setProducts(updatedProducts);
     updateCartTotal(updatedProducts);
   };
 
   const decreaseProductQuantity = (productToDecrease: ICartProduct) => {
     const updatedProducts = products.map((product: ICartProduct) => {
-      if (product.id === productToDecrease.id) {
-        product.quantity -= 1;
-      }
-
-      return product;
+      return updateQuantitySafely(product, productToDecrease, -1);
     });
 
+    setProducts(updatedProducts);
     updateCartTotal(updatedProducts);
   };
 
